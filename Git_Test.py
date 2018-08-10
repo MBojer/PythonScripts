b68b20dd231fa766370d666e5b1aa38abb7c5f6d
Morten.Bojer <morten.bojer@gmail.com>
2018-08-10 11:49:02 +0100

    test

--- Git_Test.py
+++ Git_Test.py
@@ -5,4 +5,11 @@ repo = git.Repo("https://github.com/MBojer/PythonScripts.git")
 
 print "MARKER"
 
-repo.index.diff(repo.head.commit)
+repo = git.Repo('repo_path')
+commits_list = list(repo.iter_commits())
+
+# --- To compare the current HEAD against the bare init commit
+a_commit = commits_list[0]
+b_commit = commits_list[-1]
+
+a_commit.diff(b_commit)
